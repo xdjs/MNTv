8ac896eeb3b5f6ca00ab67986b8d3c2d00aa55ab
Nathan R <nhrussell06@gmail.com>
2025-07-31 13:41:57 -0700

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link"
 
 export default function HomeScreen() {
      return(
-          <div className="center-text border-sm">
-               <p>
-                    <Link href="https://mn-tv-git-staging-musicnerd.vercel.app/sign-in">Sign In</Link>
-               </p>
+          <div className="h-screen flex justify-center items-center">
+               <button className="bg-blue-500 text-white px-4 py-2 rounded-md ">
+                         <Link href="https://mn-tv-git-staging-musicnerd.vercel.app/sign-in">Sign In or else...</Link>
+               </button>
           </div>
      )
 }
