c95c852f7eb83b279c2a4e09845d6ab558ed0b57
Nathan R <nhrussell06@gmail.com>
2025-07-31 13:26:53 -0700

    link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 
+import Link from "next/link"
+
 export default function HomeScreen() {
      return(
           <div className="center-text border-sm">
-               <p>test</p>
+               <p>
+                    <Link href="https://mn-tv-git-staging-musicnerd.vercel.app/sign-in">Sign In</Link>
+               </p>
           </div>
      )
 }
