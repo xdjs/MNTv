b3e6a51329eb6256795b482184c644a4cfcbe800
Nathan R <nhrussell06@gmail.com>
2025-08-06 17:24:51 -0700

    add test track info display

--- src/app/listening/page.tsx
+++ src/app/listening/page.tsx
@@ -1,14 +1,27 @@
 "use client"
 
-import currentTrackInfo from "../../hooks/useCurrentTrack"
+import useCurrentTrack from "@/hooks/useCurrentTrack"
 import { useSession } from "next-auth/react"
 
 
+
+
 export default function Listening() {
+
+     //for future use; can be used to notify other components that the song changed (such as slide components)
+     const handleSongChange = (track: any) => {
+          
+     }
+
+     const currentTrack = useCurrentTrack({ onSongChange: handleSongChange });
+
      return (
           <div>
                <div className="justify-center items-center">
-                    
+                    <img src={currentTrack.coverUrl!} alt="ya mum" />
+                    <h1>{currentTrack.songName}</h1>
+                    <p>{currentTrack.artistName}</p>
+                    <p>{currentTrack.albumName}</p>
                </div>
           </div>
      )
